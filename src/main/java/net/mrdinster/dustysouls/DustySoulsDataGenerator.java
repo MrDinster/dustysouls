29a5b25543cde586bfff4ee84546f3a4fdbcbf91
MrDinster <dinster455@gmail.com>
2026-08-11 22:28:13 -0400

    start

---
+++ src/main/java/net/mrdinster/dustysouls/DustySoulsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mrdinster.dustysouls;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DustySoulsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
